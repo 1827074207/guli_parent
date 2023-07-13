825c72dbef0228f0647e5fbe81c84cf0de185c2c
haoqiang <1827074207@qq.com>
2023-07-13 19:36:09 +0800

    更新课程简介

---
+++ serivce/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
@@ -0,0 +1,18 @@
+package com.atguigu.eduservice.service;
+
+import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2023-07-12
+ */
+public interface EduCourseService extends IService<EduCourse> {
+
+    void addCourse(CourseInfoVo courseInfoVo);
+}
